43ae01846e567dd40a9c4fb41897a904cc608d21
Juan Muñoz <cl.jmunoz@gmail.com>
2024-12-15 22:49:27 -0300

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 // export const BASE_API = "https://icard-django.herokuapp.com";
 
-export const BASE_API = 'https://https://api-eresto.fly.dev'
+export const BASE_API = 'https://api-eresto.fly.dev'
 //export const BASE_API = 'http://127.0.0.1:8000'
 // //# process.env.BASE_API_URL;
 export const TOKEN = "token";
